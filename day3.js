7fe90a6d5e7c8469162ad96b4f722d34d36fc2e3
vbc221 <vbc221@nyu.edu>
2019-07-10 10:52:46 -0500

    exercise 2

--- day3.js
+++ day3.js
@@ -21,3 +21,19 @@ function createMyObject(){
 }
 
   console.log(createMyObject()) ; 
+
+/*Object updater
+Modify the updateObject function (which takes a single argument (obj)) so that it adds the following key / value pairs to obj and returns it:
+
+foo => 'foo'
+bar => 'bar'
+bizz => 'bizz'
+bang => 'bang' */
+
+function updateObject(obj) {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
+}
