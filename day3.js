ac4d0a1c554ab63fedbb7f0f10bd40fc9db25675
vbc221 <vbc221@nyu.edu>
2019-07-10 11:04:17 -0500

    excercise 4

--- day3.js
+++ day3.js
@@ -56,3 +56,20 @@ function personMaker() {
   };
   return person;
 }
+
+/*Deleting keys
+Modify the keyDeleter function so that it deletes keys for foo and bar 
+for any object passed in and then returns the modified object.*/
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
