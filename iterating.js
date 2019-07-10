bd91153fbd7333907f8ec2ca84cbb0788a3b12c6
Ramarduk <ramarduk@gmail.com>
2019-07-10 10:33:10 -0600

    trying shortcut

---
+++ iterating.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/*Make student reports
+Take up to 10 minutes to write a function called makeStudentReport that takes a single argument, data.
+ data is an array of objects. 
+ Each object in the array represents a student and their letter grade for a course
+  — for example, {name: 'Johnny Robot', grade: 'C'}.
+
+makeStudentReport should return an array of strings. 
+For each item in data, return a string that looks like this: '[name]: [grade]'. 
+The name and grade values on the student object should be substituted in.*/
+
+function makeStudentReport(data) {
+  const results =[];
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i];
+    results.push(`${item.name} : ${item.grade}`);
+  }
+  return results;
+}
