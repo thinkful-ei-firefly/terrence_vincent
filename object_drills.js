ac7dd42d05669faa4ff5f960225b753f76e2dc0b
vbc221 <vbc221@nyu.edu>
2019-07-10 13:51:02 -0500

    example 2

--- object_drills.js
+++ object_drills.js
@@ -18,3 +18,22 @@ let loaf = {
 
 console.log(loaf);
 console.log(loaf.hydration());
+
+/*Iterating over an object's properties
+Create an object with five properties: foo, bar, fum, quux, and spam. 
+Give each property a unique value of your choosing.
+Loop over the object using for ... in
+Use console.log to show each property name and its associated value. */
+
+const obj={
+  foo:2,
+  bar:3,
+  fum:4,
+  quux:5,
+  spam:6,
+}
+
+for (const key in obj){
+  const value=obj[key];
+  console.log(value);
+}
